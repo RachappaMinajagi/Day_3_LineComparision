9491f3401789c69770df927a6bbba3baaef5e3b8
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-15 20:21:14 +0530

    UC_3 is Added

--- src/LineComparisonComputation.java
+++ src/LineComparisonComputation.java
@@ -34,11 +34,18 @@ public class LineComparisonComputation {
                 String val1=String.valueOf(lenthOfline_1);
                 String val2=String.valueOf(lenthOfline_2);
                 
-                
-                    int x1 = 2;
-                    int x2 = 4;
-                    int y1 = 5;
-                    int y2 = 10;
+                int x1 = 2;
+                int x2 = 4;
+                int y1 = 5;
+                int y2 = 10;
+
+                int x3 = 3;
+                int y3 = 6;
+                int x4 = 7;
+                int y4 = 14;
+
+                double a;
+                double b;
 
                     System.out.println("A Length as 2 points : " + "("+x1+ "," +y1+ ")" + "and" + "(" +x2+ "," +y2+ ")");
                     double LenthLine = Math.sqrt((x2 - x1) * (x2-x1)) + ((y2-y1) * (y2-y1));
